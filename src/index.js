01cddaad77b36f4e8dd0cca98e2ae0fd229f4d4a
Gio <glandaburo92@gmail.com>
2022-12-21 12:59:03 +0100

    add actions, reducers and config

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createAppStore } from './store/config/storeConfig';
+import { Provider } from 'react-redux';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Provider store={createAppStore()}>
+    Component
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/store/config/storeConfig.js
@@ -0,0 +1,14 @@
+import { createStore } from "redux"
+import { composeWithDevTools } from "redux-devtools-extension"
+import { rootReducer } from "../reducers/rootReducer"
+import { configureStore } from "@reduxjs/toolkit";
+
+// export const createAppStore = () =>{
+//  let store = createStore(rootReducer, composeWithDevTools())
+//  return store
+// }
+
+export const createAppStore = () =>{
+    let store =  configureStore({reducer: rootReducer});
+    return store
+   }
